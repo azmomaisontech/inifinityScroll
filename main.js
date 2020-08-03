ad9b6689606f70df453e8862a3f9783556399fe8
AZMOMAISON <mosesaizee@outlook.com>
2020-08-03 07:47:15 -0500

    Modified the JS file to make request immediately DOM LOADS

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 const container = document.getElementById("container");
 const loading = document.querySelector(".loading");
 
-getPost();
-getPost();
-getPost();
-
-window.addEventListener("scroll", () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-  console.log({ scrollTop, scrollHeight, clientHeight });
-
-  if (clientHeight + scrollTop >= scrollHeight - 5) {
-    // show the loading animation
-    showLoading();
-  }
+//First paint immediately Dom is loaded
+window.addEventListener("DOMContentLoaded", () => {
+  getPost();
+  getPost();
+  getPost();
 });
 
-function showLoading() {
-  loading.classList.add("show");
-
-  // load more data
-  setTimeout(getPost, 1000);
+// To get a random post by creating  a random number
+function getRandomNr() {
+  return Math.floor(Math.random() * 100) + 1;
 }
 
+//Fetch post from the JPH Api
 async function getPost() {
   const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${getRandomNr()}`);
   const postData = await postResponse.json();
@@ -35,10 +26,27 @@ async function getPost() {
   addDataToDOM(data);
 }
 
-function getRandomNr() {
-  return Math.floor(Math.random() * 100) + 1;
+//Perform the infinite Scroll
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  console.log({ scrollTop, scrollHeight, clientHeight });
+
+  if (clientHeight + scrollTop >= scrollHeight - 5) {
+    // show the loading animation
+    showLoading();
+  }
+});
+
+//Show loading when making a server request
+function showLoading() {
+  loading.classList.add("show");
+
+  // load more data
+  setTimeout(getPost, 1000);
 }
 
+//Display post generated to DOM
 function addDataToDOM(data) {
   const postElement = document.createElement("div");
   postElement.classList.add("blog-post");
